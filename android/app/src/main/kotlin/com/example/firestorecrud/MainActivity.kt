826653dbb123b4fa9bd642b6729a549c7a7a5b82
Pallab Chandra Das <pallab15-5698@diu.edu.bd>
2026-08-30 12:26:31 +0600

    firestore project setup completed

---
+++ android/app/src/main/kotlin/com/example/firestorecrud/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.firestorecrud
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/home_sccreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeSccreen extends StatefulWidget {
+  const new({super.key});
+
+  @override
+  State<HomeSccreen> createState() => _HomeSccreenState();
+}
+
+class _HomeSccreenState extends State<HomeSccreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
