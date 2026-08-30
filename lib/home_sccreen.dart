a2891e8add22bcf9c0b967b7596a02f221c20174
Pallab Chandra Das <pallab15-5698@diu.edu.bd>
2026-08-31 01:30:35 +0600

    fn creted to get data from firestore as strem and add

--- lib/home_sccreen.dart
+++ lib/home_sccreen.dart
@@ -1,3 +1,4 @@
+import 'package:firestorecrud/services/database_service.dart';
 import 'package:flutter/material.dart';
 
 class HomeSccreen extends StatefulWidget {
@@ -8,8 +9,43 @@ class HomeSccreen extends StatefulWidget {
 }
 
 class _HomeSccreenState extends State<HomeSccreen> {
+  final DatabaseService _databaseService = DatabaseService();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: _appBar(),
+      body: _buildUi(),
+    );
+  }
+
+  PreferredSizeWidget _appBar() {
+    return AppBar(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      title: Text("Todo", style: TextStyle(color: Colors.white)),
+    );
+  }
+
+  Widget _buildUi() {
+    return SafeArea(child: Column(children: [_messagesListView()]));
+  }
+
+  Widget _messagesListView() {
+    return SizedBox(
+      height: MediaQuery.sizeOf(context).height * .8,
+      width: MediaQuery.sizeOf(context).width,
+      child: StreamBuilder(
+        stream: _databaseService.getTodos(),
+        builder: (context, snapshot) {
+          List todos = snapshot.data?.docs ?? [];
+
+          if (todos.isEmpty) {
+            return Center(child: Text('Add A todo'));
+          }
+          print(todos);
+          return ListView();
+        },
+      ),
+    );
   }
 }
---
+++ lib/services/database_service.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firestorecrud/model/todo.dart';
+
+/// firestore collection name "todos"
+/// should be same as collection name
+const String TODO_COLLECTION_REF = 'todos';
+
+class DatabaseService {
+  /// crete firestore instance
+  final _firestore = FirebaseFirestore.instance;
+
+  /// collection references
+  ///
+  late final CollectionReference _todoref;
+  DatabaseService() {
+    //creted reference toward firestore collection
+    //that will convert collection to todo  and todo to json to pass to firestore
+
+    _todoref = _firestore
+        .collection(TODO_COLLECTION_REF)
+        .withConverter<Todo>(
+          fromFirestore: (snapshot, _) {
+            return Todo.fromJson(snapshot.data()!);
+          },
+          toFirestore: (todo, _) => todo.toJson(),
+        );
+  }
+
+  //fn to get todo from database
+
+  Stream<QuerySnapshot> getTodos() {
+    return _todoref.snapshots();
+  }
+
+// fn to add data to firestore
+  void addTodo(Todo todo) async {
+    await _todoref.add(todo);
+  }
+}
